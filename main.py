177cb2140e1c4c29613a194ada674d2940315666
lesagedonnel <dodobuderi@gmail.com>
2023-05-10 10:19:31 +0200

    add_files

---
+++ main.py
@@ -0,0 +1,11 @@
+# QUESTION I
+
+  #1.Creation d'une liste contenant 10 elements
+
+    voitures = ['Toyota', 'Nissan', 'Jeep', 'Suzuki', 'Duster', 'Tesla', 'Mercedes', 'Porsche', 'Mustang', 'Bentley']
+
+    #Affichage de ces elements
+
+    print("-----Les elements se trouvant sur notre liste sont:-----")
+    for i in voitures:
+        print(i)
